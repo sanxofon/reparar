ec5ed927adb371f02c7e3ed5745ab9399fcf0329
Santiago Chávez <sanxofon@users.noreply.github.com>
2021-12-07 11:00:09 -0300

    Versión Marzipano

---
+++ marzipano/data.js
@@ -0,0 +1,57 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-escenario-2-david",
+      "name": "escenario 2-david",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.6398076203929861,
+        "pitch": 1.0498766610129984,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.8243371481954256,
+          "pitch": 0.3426229654596735,
+          "title": "El pedo es que<br>",
+          "text": "va pasando el tiempo y tu mentalidad tiene que cambiar.<br>"
+        },
+        {
+          "yaw": -0.9,
+          "pitch": -0.3,
+          "title": "-0.9, 0.3<br>",
+          "text": "Posición marcada para referencia.<br>",
+          "image": "azotea.png",
+          "class": "azotea"
+        }
+      ]
+    }
+  ],
+  "name": "Reparar",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
