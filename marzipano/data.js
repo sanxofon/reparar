d3c42bcbd12774d4929497cb02ae46cd0df63872
Santiago Chávez <sanxofon@users.noreply.github.com>
2022-01-09 19:42:24 -0300

    Add files via upload

---
+++ marzipano/data.js
@@ -0,0 +1,164 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-escenario-david-marzipano-1",
+      "name": "El Taller",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.5756638376702927,
+        "pitch": 0.5101494910055688,
+        "fov": 1.324777205633526
+      },
+      "linkHotspots": [
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 49,
+          "iy": 1500,
+          "iw": 811,
+          "ih": 290,
+          "src": "imgtaller/titulo-david.png",
+          "text": "alert('titulo-david.png');"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 182,
+          "iy": 383,
+          "iw": 864,
+          "ih": 971,
+          "src": "animaciones/cartel-reparar.gif",
+          "text": "a2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 1564,
+          "iy": 989,
+          "iw": 336,
+          "ih": 300,
+          "src": "animaciones/muestra-gallito2.gif",
+          "text": "a1"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 919,
+          "iy": 1924,
+          "iw": 360,
+          "ih": 360,
+          "src": "animaciones/pinza.gif",
+          "text": "a2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 1995,
+          "iy": 610,
+          "iw": 400,
+          "ih": 350,
+          "src": "animaciones/cubrebocas.gif",
+          "text": "a2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 1295,
+          "iy": 1618,
+          "iw": 384,
+          "ih": 506,
+          "src": "animaciones/mano-desarmador.gif",
+          "text": "a2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 2892,
+          "iy": 2004,
+          "iw": 714,
+          "ih": 432,
+          "src": "animaciones/multimetro.gif",
+          "text": "a2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 1288,
+          "iy": 1220,
+          "iw": 811,
+          "ih": 268,
+          "src": "imgtaller/frase-1-bonito.png",
+          "text": "alert('frase-1-bonito.png');"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 2280,
+          "iy": 1611,
+          "iw": 652,
+          "ih": 229,
+          "src": "imgtaller/frase-2-pedo.png",
+          "text": "2"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 3529,
+          "iy": 852,
+          "iw": 810,
+          "ih": 287,
+          "src": "imgtaller/frase-3-algo.png",
+          "text": "3"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 3773,
+          "iy": 1680,
+          "iw": 608,
+          "ih": 268,
+          "src": "imgtaller/frase-4-ojala.png",
+          "text": "4"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "ix": 4037,
+          "iy": 1282,
+          "iw": 837,
+          "ih": 246,
+          "src": "imgtaller/frase-5-cuando.png",
+          "text": "5"
+        }
+      ]
+    }
+  ],
+  "name": "El Taller",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
